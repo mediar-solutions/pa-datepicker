d93bdd493e07a6af1511e6f0208e7d471b5ee0f7
Denis Lins <denislins@hotmail.com>
2015-03-04 16:10:30 -0300

    Adding more datepicker tests

--- spec/directives/datepicker.single.directive.spec.js
+++ spec/directives/datepicker.single.directive.spec.js
@@ -1,5 +1,21 @@
 describe('directive: pa-date-input', function() {
 
+  var getDayCells = function(element) {
+    var cells = [];
+
+    element.find('tbody tr').each(function() {
+      var days = [];
+
+      $(this).find('td').each(function() {
+        days.push($(this).text().replace(/\s/g, ''));
+      });
+
+      cells.push(days);
+    });
+
+    return cells;
+  };
+
   beforeEach(function() {
     module('pa-datepicker');
   });
@@ -37,6 +53,13 @@ describe('directive: pa-date-input', function() {
     it('marks current day', function() {
       var expected = this.dateFilter(this.today, 'dd');
       expect(this.element.find('td.today')).toHaveText(expected);
+    });
+
+    it('shows the correct days-of-week', function() {
+      var cells = this.element.find('tr:nth-child(2) th').text();
+      var weekdays = cells.replace(/\s/g, '');
+
+      expect(weekdays).toBe('SMTWTFS');
     });
 
     it('shows the navigation buttons', function() {
@@ -80,10 +103,8 @@ describe('directive: pa-date-input', function() {
 
   describe('when the date is already set', function() {
 
-    beforeEach(inject(function($rootScope, $compile, $filter) {
+    beforeEach(inject(function($rootScope, $compile) {
       this.element = angular.element('<pa-datepicker ng-model="date"></pa-datepicker>');
-
-      this.dateFilter = $filter('date');
 
       this.scope = $rootScope.$new();
       this.scope.date = new Date('2015-04-15 00:00:00');
@@ -96,6 +117,18 @@ describe('directive: pa-date-input', function() {
 
     it('starts in the selected month', function() {
       expect(this.element.find('.active-month')).toHaveText('April 2015');
+    });
+
+    it('shows the right days', function() {
+      var cells = getDayCells(this.element);
+
+      expect(cells).toEqual([
+        ['29', '30', '31', '01', '02', '03', '04'],
+        ['05', '06', '07', '08', '09', '10', '11'],
+        ['12', '13', '14', '15', '16', '17', '18'],
+        ['19', '20', '21', '22', '23', '24', '25'],
+        ['26', '27', '28', '29', '30', '01', '02'],
+      ]);
     });
 
     it('marks the current date as selected', function() {
@@ -121,6 +154,77 @@ describe('directive: pa-date-input', function() {
 
       expect(this.scope.date.getTime()).toBe(expected);
       expect(this.element.find('td.selected')).toHaveText('15');
+    });
+
+  });
+
+  describe('overriding default options', function() {
+
+    beforeEach(inject(function($rootScope, $compile, paDatepickerConfig) {
+      this.configBackup = angular.extend({}, paDatepickerConfig);
+
+      angular.extend(paDatepickerConfig, {
+        startingDay: 2,
+        minDate: new Date('2015-04-10 00:00:00'),
+        maxDate: new Date('2015-04-20 00:00:00'),
+      });
+
+      this.element = angular.element('<pa-datepicker ng-model="date"></pa-datepicker>');
+
+      this.scope = $rootScope.$new();
+      this.scope.date = new Date('2015-04-15 00:00:00');
+
+      $compile(this.element)(this.scope);
+      this.scope.$digest();
+
+      angular.element('body').append(this.element);
+    }));
+
+    afterEach(inject(function(paDatepickerConfig) {
+      angular.extend(paDatepickerConfig, this.configBackup);
+    }));
+
+    describe('startingDay', function() {
+
+      it('shows the correct days-of-week', function() {
+        var cells = this.element.find('tr:nth-child(2) th').text();
+        var weekdays = cells.replace(/\s/g, '');
+
+        expect(weekdays).toBe('TWTFSSM');
+      });
+
+      it('shows the right days', function() {
+        var cells = getDayCells(this.element);
+
+        expect(cells).toEqual([
+          ['31', '01', '02', '03', '04', '05', '06', ],
+          ['07', '08', '09', '10', '11', '12', '13', ],
+          ['14', '15', '16', '17', '18', '19', '20', ],
+          ['21', '22', '23', '24', '25', '26', '27', ],
+          ['28', '29', '30', '01', '02', '03', '04', ],
+        ]);
+      });
+
+    });
+
+    describe('minDate/maxDate', function() {
+
+      it('disables dates lower than April 10', function() {
+        expect(this.element.find('tr:nth-child(2) td:nth-child(2)')).toHaveClass('disabled');
+        expect(this.element.find('tr:nth-child(1) td:nth-child(5)')).toHaveClass('disabled');
+      });
+
+      it('keeps valid dates selectable', function() {
+        expect(this.element.find('tr:nth-child(2) td:nth-child(4)')).not.toHaveClass('disabled');
+        expect(this.element.find('tr:nth-child(3) td:nth-child(2)')).not.toHaveClass('disabled');
+        expect(this.element.find('tr:nth-child(3) td:nth-child(7)')).not.toHaveClass('disabled');
+      });
+
+      it('disables dates higher than April 10', function() {
+        expect(this.element.find('tr:nth-child(5) td:nth-child(2)')).toHaveClass('disabled');
+        expect(this.element.find('tr:nth-child(4) td:nth-child(5)')).toHaveClass('disabled');
+      });
+
     });
 
   });
