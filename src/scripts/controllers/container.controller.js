f61311415775279eb646da3af6740f08b1306fed
Leandro Daldegam <leandro@daldegam.com.br>
2015-03-19 11:53:56 -0300

    additional check for invalid values are assigned with undefined

--- src/scripts/controllers/container.controller.js
+++ src/scripts/controllers/container.controller.js
@@ -67,6 +67,8 @@
           } else if (typeof(this.ngModel) === 'string' || this.ngModel instanceof String) {
             this.ngModel = new Date(this.ngModel);
             this.ngModel.setHours(0, 0, 0, 0);
+          } else {
+            this.ngModel = undefined;
           }
         },
 
